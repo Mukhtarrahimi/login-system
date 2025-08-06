9417b55a4dd6f649b66b617b003405c8916a1a13
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 11:32:23 +0430

    basic code of tkinter & login_system class.

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from tkinter import  *
 from tkinter import ttk
-
+# class login
+class LoginSystem():
+    def __init__(self, root):
+        print('Hello programer world')
+        
 
 root = Tk()
+root.title("Login System")
+obj = LoginSystem(root)
+root.mainloop()
