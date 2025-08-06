bb777a9af62d153b47208b978545ccc65a19e21d
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 11:42:38 +0430

    all image import.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ class LoginSystem():
         self.root.title('Login System')
         self.root.geometry('1350x700+0+0')
 
+        #  All image
+        self.bg_icon = ImageTk.PhotoImage(file="images\bg.jpg") 
+        self.user_icon = PhotoImage(file="images\user.png")
+        self.password_icon = PhotoImage(file="images\password.png")
 root = Tk()
 obj = LoginSystem(root)
 root.mainloop()
