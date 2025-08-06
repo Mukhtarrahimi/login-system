cfa12278deb55fbf9355d10ab282794599486086
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 11:47:19 +0430

    title & bg added.

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ class LoginSystem():
         self.bg_icon = ImageTk.PhotoImage(file="images\bg.jpg") 
         self.user_icon = PhotoImage(file="images\user.png")
         self.password_icon = PhotoImage(file="images\password.png")
+        
+        bg_label = Label(self.root, image=self.bg_icon). pack()
+        title = Label(self.root, text="Login System", font=("times new roman", 40, "bold"),bg='yellow', fg='red', bd=10, relief=GROOVE)
+        title.place(x=0, y=0, relwidth=0)
+        
+        
 root = Tk()
 obj = LoginSystem(root)
 root.mainloop()
