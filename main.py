847a4b759c07b5749811d2fcab3cca0e5bc72bb8
Mukhtar Rahimi <mukhtarrahimi110@gmail.com>
2025-08-06 13:34:59 +0430

    login btn.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class LoginSystem():
         login_frame = Frame(self.root, bg='white')
         login_frame.place(x = 400, y = 150)
         
-        logo_label = Label(login_frame, image=self.user_icon).grid(row=0, column=0, pady=20)
+        logo_label = Label(login_frame, image=self.user_icon, bd=0).grid(row=0, columnspan=1, pady=20)
         
         user_label = Label(login_frame, text="Username", compound=LEFT, font=("times new roman", 15, "bold"), bg='white').grid(row = 1, column=0, padx=20, pady=20)
         user_input = Entry(login_frame, bd=5, relief=GROOVE, font=("times new roman", 15, "bold"), bg='light').grid(row=1, column=1, padx=20)
@@ -30,6 +30,7 @@ class LoginSystem():
         password_label = Label(login_frame, text="Password", compound=LEFT, font=("times new roman", 15, "bold"))
         password_input = Entry(login_frame, bd=5, relief=GROOVE, font=("times new roman", 15, "bold"), bg='light').grid(row=2, column=1, padx=20)
         
+        login_btn = Button(login_frame, text='Login', font=("times new roman", 15, "bold"), bg='green', width=15, height=3)
         
         
         
